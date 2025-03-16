5c5daff5e674c965525075dd1e48e45e81acb43c
vmanidev <vmanidev@outlook.com>
2025-03-16 15:44:53 +0530

    refactor code

--- app.js
+++ app.js
@@ -2,66 +2,83 @@ import constants from "./constants.js";
 
 class App {
 
-    loadUnitListEle() {
-        this.getUnitListData()
-            .then(unitList => {
-                unitList.forEach(unit => {
-                    const unitEle = document.createElement('div');
-                    const i = document.createElement('i');
-                    const span = document.createElement('span');
+    elements = {
+        loadUnitListEle() {
+            app.getUnitListData()
+                .then(unitList => {
+                    unitList.forEach(unit => {
+                        const unitEle = document.createElement('div');
+                        const i = document.createElement('i');
+                        const span = document.createElement('span');
 
-                    unitEle.id = unit.id;
-                    i.classList.add(...unit.icon.split(' '));
-                    unitEle.classList.add('unit-thumb');
-                    span.textContent = unit.name;
+                        unitEle.id = unit.id;
+                        i.classList.add(...unit.icon.split(' '));
+                        unitEle.classList.add('unit-thumb');
+                        span.textContent = unit.name;
 
-                    //appending unit item into unit list container
-                    unitEle.append(i, span);
-                    constants.ELEMENTS.unitList.appendChild(unitEle);
+                        //appending unit item into unit list container
+                        unitEle.append(i, span);
+                        constants.ELEMENTS.unitList.appendChild(unitEle);
+                    })
                 })
-            })
-            .catch(error => this.showErrorBanner(error, constants.ELEMENTS.unitList));
+                .catch(error => this.showErrorBanner(error, constants.ELEMENTS.unitList));
 
-    }
+        },
 
-    loadUnitConverterEle(target) {
-        const unitType = target.id ? target.id : target.parentElement.id;
-        this.getMetricUnits()
-            .then(metricUnits => {
-                this.createUnitConverterEle(metricUnits[unitType]);
-            })
-            .catch(error => this.showErrorBanner(error, constants.ELEMENTS.unitConverter));
-    }
+        loadUnitConverterEle(target) {
+            const unitType = target.id ? target.id : target.parentElement.id;
+            app.getMetricUnits()
+                .then(metricUnits => {
+                    this.createUnitConverterEle(metricUnits[unitType]);
+                })
+                .catch(error => this.showErrorBanner(error, constants.ELEMENTS.unitConverter));
+        },
 
-    createUnitConverterEle(metricUnits) {
-        constants.ELEMENTS.unitConverter.innerHTML = '';
+        createUnitConverterEle(metricUnits) {
+            constants.ELEMENTS.unitConverter.innerHTML = '';
 
-        const input1 = this.createInputField();
-        const input2 = this.createInputField();
+            const input1 = this.createInputField();
+            const input2 = this.createInputField();
 
-        const i = document.createElement('i');
-        i.classList.add('fa-solid', 'fa-right-left');
+            const i = document.createElement('i');
+            i.classList.add('fa-solid', 'fa-right-left');
+            i.id = 'convert-icon';
 
-        const dropdownForInput1 = this.createDropdown(metricUnits);
-        const dropdownForInput2 = this.createDropdown(metricUnits);
+            const dropdownForInput1 = this.createDropdown(metricUnits);
+            const dropdownForInput2 = this.createDropdown(metricUnits);
 
-        constants.ELEMENTS.unitConverter.append(input1, dropdownForInput1, i, input2, dropdownForInput2);
-    }
+            constants.ELEMENTS.unitConverter.append(input1, dropdownForInput1, i, input2, dropdownForInput2);
+        },
 
-    createInputField() {
-        const input = document.createElement('input');
-        input.placeholder = 'Enter value to convert';
-        return input;
-    }
+        createInputField() {
+            const input = document.createElement('input');
+            input.placeholder = 'Enter value to convert';
+            return input;
+        },
 
-    createDropdown(metricUnits) {
-        const dropdown = document.createElement('select');
-        metricUnits.forEach(metricUnit => {
-            const option = document.createElement('option');
-            option.textContent = metricUnit.unit;
-            dropdown.appendChild(option);
-        });
-        return dropdown;
+        createDropdown(metricUnits) {
+            const dropdown = document.createElement('select');
+            metricUnits.forEach(metricUnit => {
+                const option = document.createElement('option');
+                option.textContent = metricUnit.unit;
+                dropdown.appendChild(option);
+            });
+            return dropdown;
+        },
+
+        showHideUnitConverterList(show) {
+            constants.ELEMENTS.unitList.style.display = show ? 'grid' : 'none';
+            constants.ELEMENTS.unitConverter.style.display = show ? 'none' : 'grid';
+            constants.ELEMENTS.homeBtn.style.display = show ? 'none' : 'block';
+        },
+
+        showErrorBanner(errorInfo, target) {
+            target.innerHTML = '';
+            const errorEle = document.createElement('pre');
+            errorEle.classList.add('error');
+            errorEle.textContent = errorInfo;
+            target.appendChild(errorEle);
+        }
     }
 
     async getUnitListData() {
@@ -74,20 +91,6 @@ class App {
         const response = await fetch(constants.URLS.metricUnits);
         const metricUnits = await response.json();
         return metricUnits;
-    }
-
-    showHideUnitConverterList(show) {
-        constants.ELEMENTS.unitList.style.display = show ? 'grid' : 'none';
-        constants.ELEMENTS.unitConverter.style.display = show ? 'none' : 'grid';
-        constants.ELEMENTS.homeBtn.style.display = show ? 'none' : 'block';
-    }
-
-    showErrorBanner(errorInfo, target) {
-        target.innerHTML = '';
-        const errorEle = document.createElement('pre');
-        errorEle.classList.add('error');
-        errorEle.textContent = errorInfo;
-        target.appendChild(errorEle);
     }
 }
 
--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import { app } from "./app.js";
 import constants from "./constants.js";
 
 const init = () => {
-    app.loadUnitListEle();
+    app.elements.loadUnitListEle();
 
     constants.ELEMENTS.app.addEventListener('click', e => handleClickEvent(e.target));
 
     const handleClickEvent = target => {
         if (target === constants.ELEMENTS.homeBtn || target.parentElement.id === constants.ELEMENTS.homeBtn.id) {
-            app.showHideUnitConverterList(true);
+            app.elements.showHideUnitConverterList(true);
         } else if (constants.UNITS.some(id => id === target.id || id === target.parentElement.id)) {
-            app.showHideUnitConverterList(false);
-            app.loadUnitConverterEle(target);
+            app.elements.showHideUnitConverterList(false);
+            app.elements.loadUnitConverterEle(target);
         }
     }
 }
